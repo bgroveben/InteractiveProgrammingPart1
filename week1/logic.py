f0e12ef4b9eba60aefa3547c2173c275028c7be9
bgroveben <bgroveben@gmail.com>
2017-04-15 02:24:37 -0700

    Logic and Comparisons

---
+++ week1/logic.py
@@ -0,0 +1,19 @@
+# Logic and Booleans 
+
+a = True
+b = False
+c = True
+d = False
+
+print a
+print b
+
+print "===="
+
+print not a
+print a and b
+print a or b
+
+print "===="
+
+print (a and b) or (c and (not d))
