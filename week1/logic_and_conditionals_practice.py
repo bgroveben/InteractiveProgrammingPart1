674d881da8e7b2833ee2d66c47c1c387ba6a9aec
bgroveben <bgroveben@gmail.com>
2017-04-19 08:12:32 -0700

    Continuing Practice Exercises for Logic and Conditionals

--- week1/logic_and_conditionals_practice.py
+++ week1/logic_and_conditionals_practice.py
@@ -232,6 +232,128 @@ print
 
 
 
+# 5.
+# Compute whether two intervals intersect.
+
+###################################################
+# Interval intersection formula
+# Student should enter function on the next lines.
+
+def interval_intersect(a, b, c, d):
+    """
+    (int, int, int, int) -> bool
+
+    Write a Python function interval_intersect that takes parameters a, b, c, and d and returns
+    True if the intervals [a,b] and [c,d] intersect and False otherwise.
+    In other words, if there are any numbers that are in both [a,b] and [c,d], return True,
+    False otherwise.
+
+    Precondition: (a <= b) and (c <= d)
+
+    >>> interval_intersect(0, 1, 1, 2)
+    True
+    >>> interval_intersect(1, 2, 0, 1)
+    True
+    >>> interval_intersect(0, 1, 2, 3)
+    False
+    >>> interval_intersect(2, 3, 0, 1)
+    False
+    >>> interval_intersect(0, 3, 1, 2)
+    True
+    """
+    return (b >= c) and (a <= d)
+
+###################################################
+# Tests
+# Student should not change this code.
+
+def test(a, b, c, d):
+    """Tests the interval_intersect function."""
+    print "Intervals [" + str(a) + ", " + str(b) + "] and [" + str(c) + ", " + str(d) + "]",
+    if interval_intersect(a, b, c, d):
+        print "intersect."
+    else:
+        print "do not intersect."
+
+test(0, 1, 1, 2)
+test(1, 2, 0, 1)
+test(0, 1, 2, 3)
+test(2, 3, 0, 1)
+test(0, 3, 1, 2)
+print
+
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#Intervals [0, 1] and [1, 2] intersect.
+#Intervals [1, 2] and [0, 1] intersect.
+#Intervals [0, 1] and [2, 3] do not intersect.
+#Intervals [2, 3] and [0, 1] do not intersect.
+#Intervals [0, 3] and [1, 2] intersect.
+
+
+
+# 6.
+# Compute the statement about a person's name and age, given the person's name and age.
+
+###################################################
+# Name and age formula
+# Student should enter function on the next lines.
+
+def name_and_age(name, age):
+    """
+    (str, num) -> str
+
+    Write a Python function name_and_age that takse as input the parameters name (a string)
+    and age (a number) and returns a string of the form "% is % years old." where the percents
+    are the string forms of name and age.
+    The function should include an error check for the case when age is less than zero.
+    If age is less than zero, the function should return the string "Error: Invalid Age"
+
+    >>> name_and_age('Ben', 27)
+    'Ben is 27 years old.'
+    >>> name_and_age('Dave', -1)
+    'Error: Invalid age'
+    >>> name_and_age('Tavo', 22)
+    'Tavo is 22 years old.'
+    """
+    if age < 0:
+        return "Error: Invalid age"
+    else:
+        return name + " is " + str(age) + " years old."
+
+
+###################################################
+# Tests
+# Student should not change this code.
+
+def test(name, age):
+	"""Tests the name_and_age function."""
+
+	print name_and_age(name, age)
+
+test("Joe Warren", 52)
+test("Scott Rixner", 40)
+test("John Greiner", -46)
+print
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#Joe Warren is 52 years old.
+#Scott Rixner is 40 years old.
+#Error: Invalid age
+
+
+
+# 7.
+
+
+
+
 
 
 
