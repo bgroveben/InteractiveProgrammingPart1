dd7572d914a3efaa1471d6a85c4e4753c9fcc6dd
bgroveben <bgroveben@gmail.com>
2018-08-24 12:55:21 -0700

    Add example of a single-word word cloud

---
+++ examples/single_word.py
@@ -0,0 +1,20 @@
+# https://amueller.github.io/word_cloud/auto_examples/single_word.html#sphx-glr-auto-examples-single-word-py
+
+import numpy as np
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+text = "WordCloud"
+
+x, y = np.ogrid[:300, :300]
+
+mask = (x - 150) ** 2 + (y - 150) ** 2 > 130 ** 2
+mask = 255 * mask.astype(int)
+
+
+wc = WordCloud(background_color="white", repeat=True, mask=mask)
+wc.generate(text)
+
+plt.axis("off")
+plt.imshow(wc, interpolation="bilinear")
+plt.show()
